c8402586178de82a1495262f9e7c88e6194d7218
Christian J. Schweinhardt <are.u.kidding@me.com>
2015-01-09 12:10:49 +0100

    Added some features

--- widget.py
+++ widget.py
@@ -3,7 +3,7 @@
 # @Author: chschw
 # @Date:   2015-01-08 11:24:33
 # @Last Modified by:   chschw
-# @Last Modified time: 2015-01-08 14:44:45
+# @Last Modified time: 2015-01-09 10:39:47
 
 
 from ajenti.api import plugin
@@ -20,28 +20,50 @@ class SiteMonitorWidget (ConfigurableWidget):
 
     def on_start(self):
         self.find('website').text = self.config['website']
-        status='ban-circle' # ok / remove
 
+        # Default value is Error
+        status='ban-circle' # Icons: ok-sign (Success) / ban-circle (Error)
+        p = 'http'
+
+        if self.config['protocoll'] is True:
+            self.find('type').icon = 'key'
+            p = 'https'
+
+        # If website url is given
         if self.config['website'] != '':
             import httplib2
-            h = httplib2.Http(".cache")
+            import datetime
 
+            h = httplib2.Http(".cache")
+            time = datetime.datetime.now().strftime('%d.%m.%Y at %H:%M:%S')
+
+            # Try to make an request to website
             try:
-                valid_status = [200, 301, 302] 
-                resp, content = h.request('http://' + self.config['website'])
-                print resp.status
+                valid_status = [200, 301, 302]
+                resp, content = h.request(p + '://' + self.config['website'])
+
+                # If website is available has a valid status
                 if resp.status in valid_status:
                     status = 'ok-sign'
             except:
                 pass
 
+        # Write icon in context
         self.find('status').icon = status
+        self.find('checkdate').text = 'Last check on ' + time
 
     def create_config(self):
-        return {'website': ''}
+        print 'create config'
+        return {
+            'website': '',
+            'protocoll': False
+            }
 
     def on_config_start(self):
-        pass
+        self.dialog.find('website').value = self.config.get('website', '')
+        self.dialog.find('protocoll').value = self.config.get('protocoll', False)
 
     def on_config_save(self):
         self.config['website'] = self.dialog.find('website').value
+        self.config['protocoll'] = self.dialog.find('protocoll').value
+
